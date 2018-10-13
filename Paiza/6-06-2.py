b8e6ad9d171fce989aa8da46ef80aa7c639d5a5c
kotaro-sakai1941 <43873987+kotaro-sakai1941@users.noreply.github.com>
2018-10-13 23:18:06 +0900

    Update Paiza/6-06-2.py

---
+++ Paiza/6-06-2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 13 23:16:56 2018
+
+@author: sakai kotaro
+"""
+
+# 2次元リストを作成してみよう
+numbers = [[7 for i in range(4)] for j in range(5)]
+print(numbers)
