29735966b2747b206574031a701cdbadcd0afb7e
kotaro-sakai1941 <43873987+kotaro-sakai1941@users.noreply.github.com>
2018-10-14 22:45:22 +0900

    Update Paiza/8-05-2.py

---
+++ Paiza/8-05-2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 22:44:27 2018
+
+@author: sakai kotaro
+"""
+
+# coding: utf-8
+# 学生メソッドを作る
+
+class Gakusei:
+    def __init__(self, kokugo, sansu):
+        self.kokugo = kokugo
+        self.sansu  = sansu
+
+    # この下に、合計得点を戻り値として返すsumメソッドを記述する
+    def sum(self):
+        return self.kokugo + self.sansu
+        
+yamada = Gakusei(70, 43)
+print("合計は" + str(yamada.sum()) + "点です")
